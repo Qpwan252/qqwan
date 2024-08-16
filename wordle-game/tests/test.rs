3796dea37e62fe6d8ac783effa65c0c3c1085dcf
Qpwan252 <pows007@outlook.com>
2024-08-16 12:49:51 +0800

    Update test.rs

--- wordle-game/tests/test.rs
+++ wordle-game/tests/test.rs
@@ -7,22 +7,8 @@ const GAME_SESSION_ID: u64 = 2;
 const USER1: u64 = 10;
 const USER2: u64 = 11;
 
-fn setup() -> System {
-    let sys = System::new();
-    sys.init_logger();
 
-    let wordle = Program::from_file(&sys, "../target/wasm32-unknown-unknown/debug/wordle.wasm");
-    let game_session =
-        Program::from_file(&sys, "../target/wasm32-unknown-unknown/debug/session.wasm");
-
-    let user_id: ActorId = USER1.into();
-    let wordle_id: ActorId = WORDLE_ID.into();
-    assert!(!wordle.send(user_id, wordle_id).main_failed());
-    assert!(!game_session.send(user_id, wordle_id).main_failed());
-    return sys;
-}
-
-// #[test]
+ #[test]
 fn test_win() {
     let sys = setup();
     let game_session = sys.get_program(GAME_SESSION_ID).unwrap();
@@ -127,7 +113,7 @@ fn test_win() {
     assert_eq!(session.result, SessionResult::Lose);
 }
 
-// #[test]
+ #[test]
 fn test_lose_with_too_many_check() {
     let sys: System = setup();
     let game_session = sys.get_program(GAME_SESSION_ID).unwrap();
@@ -227,4 +213,19 @@ fn test_lose_with_timeout() {
         SessionStatus::StartGameWaiting
     );
     assert_eq!(state.user_sessions[0].1.result, SessionResult::Lose);
+}
+
+fn setup() -> System {
+    let sys = System::new();
+    sys.init_logger();
+
+    let wordle = Program::from_file(&sys, "../target/wasm32-unknown-unknown/debug/wordle.wasm");
+    let game_session =
+        Program::from_file(&sys, "../target/wasm32-unknown-unknown/debug/session.wasm");
+
+    let user_id: ActorId = USER1.into();
+    let wordle_id: ActorId = WORDLE_ID.into();
+    assert!(!wordle.send(user_id, wordle_id).main_failed());
+    assert!(!game_session.send(user_id, wordle_id).main_failed());
+    return sys;
 }
